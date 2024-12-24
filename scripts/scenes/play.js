080c537e159392652f133ef29f22be03c55244a3
lomiiiii <lomenova.ivana@gmail.com>
2024-12-24 16:07:27 +0100

    fixed tile generating and added 2 levels

--- scripts/scenes/play.js
+++ scripts/scenes/play.js
@@ -8,14 +8,25 @@ export default class PlayScene extends Phaser.Scene {
   preload() {
     console.log('Preloading assets...');
     // Načítanie všetkých sprite súborov z assets
-    this.load.image('brickSprite', 'assets/images/tiles/tileSlimGray0.png'); // Východzí sprite
+    this.load.image('tileSlimGray0', 'assets/images/tiles/tileSlimGray0.png'); // Východzí sprite
+    this.load.image('tileSlimGreen0', 'assets/images/tiles/tileSlimGreen0.png');
+    this.load.image('tileSlimYellow0', 'assets/images/tiles/tileSlimYellow0.png');
+    this.load.image('tileSlimOrange0', 'assets/images/tiles/tileSlimOrange0.png');
+    this.load.image('tileSlimRed0', 'assets/images/tiles/tileSlimRed0.png');
+    this.load.image('tileSlimPurple0', 'assets/images/tiles/tileSlimPurple0.png');
+    this.load.image('tileThickGray0', 'assets/images/tiles/tileThickGray0.png');
+    this.load.image('tileThickGreen0', 'assets/images/tiles/tileThickGreen0.png');
+    this.load.image('tileThickYellow0', 'assets/images/tiles/tileThickYellow0.png');
+    this.load.image('tileThickOrange0', 'assets/images/tiles/tileThickOrange0.png');
+    this.load.image('tileThickRed0', 'assets/images/tiles/tileThickRed0.png');
+    this.load.image('tileThickPurple0', 'assets/images/tiles/tileThickPurple0.png');
   }
 
   create() {
     console.log('Creating PlayScene...');
     // Vytvorenie inštancie LevelLoader a načítanie úrovne
     this.levelLoader = new LevelLoader(this);
-    this.levelLoader.loadLevel(3); // Načítanie prvej úrovne
+    this.levelLoader.loadLevel(2); // Načítanie prvej úrovne
     console.log('PlayScene created!');
   }
 
