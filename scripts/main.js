1be906879c3249c7c5973db788dfbea9e21e54e1
McLehotsky <mclehotsky@gmail.com>
2025-01-04 20:47:13 +0100

    pwafix4

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ const game = new Phaser.Game(config);
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
       navigator.serviceWorker
-          .register('/service-worker.js')
+          .register('./service-worker.js')
           .then((registration) => {
               console.log('Service Worker registered with scope:', registration.scope);
           })
