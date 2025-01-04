2ada5ff6f7bbfdec6ea339be71800d30a485fbc3
lomiiiii <lomenova.ivana@gmail.com>
2025-01-04 21:48:37 +0100

    added ui sounds and buttons

--- scripts/scenes/pause.js
+++ scripts/scenes/pause.js
@@ -5,10 +5,21 @@ export default class PauseMenu extends Phaser.Scene {
 
     preload()
     {
+        this.load.image('playButtonSmall', 'assets/images/ui/PlayButtonSmall.png');
+        this.load.image('playButtonSmallPressed', 'assets/images/ui/PlayButtonSmallPressed.png');
+        this.load.image('restartButton', 'assets/images/ui/RestartButton.png');
+        this.load.image('restartButtonPressed', 'assets/images/ui/RestartButtonPressed.png');
+        this.load.image('homeButton', 'assets/images/ui/HomeButton.png');
+        this.load.image('homeButtonPressed', 'assets/images/ui/HomeButtonPressed.png');
+        
         this.add.text(0, 0, '', { fontFamily: 'm6x11', fontSize: '16px' });
+
+        this.load.audio('click', 'assets/sounds/mixkit-mouse-click-close-1113.wav');
     }
 
     create() {
+        const { width, height } = this.scale; // Získanie šírky a výšky scény
+
         // Získanie stredu obrazovky
         const centerX = this.scale.width / 2;
         const centerY = this.scale.height / 2;
@@ -19,33 +30,73 @@ export default class PauseMenu extends Phaser.Scene {
         // Text "PAUSED"
         this.add.text(centerX, centerY - 100, 'PAUSED', { fontSize: '48px', fill: '#ffffff', fontFamily: 'm6x11' }).setOrigin(0.5);
 
-        // Tlačidlo Resume
-        const resumeButton = this.add.text(centerX, centerY, 'Resume', { fontSize: '32px', fill: '#ffffff' })
-            .setOrigin(0.5)
-            .setInteractive()
-            .on('pointerdown', () => {
-                this.scene.stop(); // Zastav PauseMenu scénu
-                this.scene.resume('PlayScene'); // Obnov hernú scénu
-            });
+        this.clickSound = this.sound.add('click');
+
+        // Pridanie tlačidla na stred obrazovky pod text
+        const resumeButton = this.add.sprite(width / 2 - 50, height / 2 + 20, 'playButtonSmall').setInteractive();
+        resumeButton.setOrigin(0.5); // Nastavenie stredu tlačidla ako referenčného bodu
+
+        // Spracovanie udalostí pre tlačidlo
+        resumeButton.on('pointerdown', () => {
+            resumeButton.setTexture('playButtonSmallPressed'); // Zmena na stlačený sprite
+        });
+
+        resumeButton.on('pointerup', () => {
+            this.clickSound.play();
+            resumeButton.setTexture('playButtonSmall'); // Zmena späť na normálny sprite
+            this.scene.stop();
+            this.scene.resume('PlayScene'); // Prepnutie na PlayScene pri uvoľnení tlačidla
+        });
+
+        resumeButton.on('pointerout', () => {
+            resumeButton.setTexture('playButtonSmall'); // Zmena späť na normálny sprite, ak ukazovateľ opustí tlačidlo
+        });
 
         // Tlačidlo Restart
-        const restartButton = this.add.text(centerX, centerY + 50, 'Restart', { fontSize: '32px', fill: '#ffffff' })
-            .setOrigin(0.5)
-            .setInteractive()
-            .on('pointerdown', () => {
-                this.scene.stop(); // Zastav PauseMenu scénu
-                this.scene.stop('PlayScene'); // Zastav GameScene
-                this.scene.start('PlayScene'); // Reštartuj hru
-            });
+        const restartButton = this.add.sprite(width / 2, height / 2 + 20, 'restartButton').setInteractive();
+        restartButton.setOrigin(0.5); // Nastavenie stredu tlačidla ako referenčného bodu
+
+        // Spracovanie udalostí pre tlačidlo
+        restartButton.on('pointerdown', () => {
+            restartButton.setTexture('restartButtonPressed'); // Zmena na stlačený sprite
+        });
+
+        restartButton.on('pointerup', () => {
+            this.clickSound.play();
+            restartButton.setTexture('restartButton'); // Zmena späť na normálny sprite
+            this.scene.stop();
+            this.scene.stop('PlayScene');
+            this.scene.start('PlayScene'); // Prepnutie na PlayScene pri uvoľnení tlačidla
+        });
+
+        restartButton.on('pointerout', () => {
+            restartButton.setTexture('restartButton'); // Zmena späť na normálny sprite, ak ukazovateľ opustí tlačidlo
+        });
 
         // Tlačidlo Main Menu
-        const mainMenuButton = this.add.text(centerX, centerY + 100, 'Main Menu', { fontSize: '32px', fill: '#ffffff' })
-            .setOrigin(0.5)
-            .setInteractive()
-            .on('pointerdown', () => {
-                this.scene.stop(); // Zastav PauseMenu scénu
-                this.scene.stop('PlayScene'); // Zastav GameScene
-                this.scene.start('GameScene'); // Spusti hlavnú obrazovku
-            });
+        const homeButton = this.add.sprite(width / 2 + 50, height / 2 + 20, 'homeButton').setInteractive();
+        homeButton.setOrigin(0.5); // Nastavenie stredu tlačidla ako referenčného bodu
+      
+        // Spracovanie udalostí pre tlačidlo
+        homeButton.on('pointerdown', () => {
+            homeButton.setTexture('homeButtonPressed'); // Zmena na stlačený sprite
+        });
+      
+        homeButton.on('pointerup', () => {
+            this.clickSound.play();
+            homeButton.setTexture('homeButton'); // Zmena späť na normálny sprite
+            this.scene.stop('PlayScene');
+            this.scene.start('GameScene'); // Prepnutie na GameScene pri uvoľnení tlačidla
+        });
+      
+        homeButton.on('pointerout', () => {
+            homeButton.setTexture('homeButton'); // Zmena späť na normálny sprite, ak ukazovateľ opustí tlačidlo
+        });
+
+        this.children.list.forEach((child) => {
+            if (child instanceof Phaser.GameObjects.Sprite) {
+                child.setScale(1.5); // Zväčšenie spriteov
+            }
+        });
     }
 }
